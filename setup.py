280a14e971df4cc9c896624362fb935ab8af1d8b
Ryan Sjostrand <rsjostrand@protonmail.com>
2016-05-26 23:58:05 -0500

    Converted to a ROS package

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='lib_crio',
+	version='0.0.0',
+	description='2009-2014 FRC Driver station protocol',
+	author='Ryan Sjostrand',
+	author_email='rsjostrand@protonmail.com',
+	packages=['crio'],
+	package_dir={'' : 'scripts'}
+	)
